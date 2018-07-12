1d46636ea3df702b1b42c544fce954c187be1c03
Andy Liu <andyliuliming@outlook.com>
2018-07-12 16:36:47 +0800

    fix.

--- src/bosh_azure_cpi/spec/manual_tests/performance/performance.rb
+++ src/bosh_azure_cpi/spec/manual_tests/performance/performance.rb
@@ -1,3 +1,7 @@
+
+
+cpi.storage_account_manager.get_storage_account_from_resource_pool(cpi.options['azure'], 'eastasia')
+
 stemcell_properties = {
   "name" => "fake-name",
   "version" => "fake-version",
@@ -9,14 +13,24 @@ stemcell_properties = {
   "os_type" => "linux",
   "os_distro" => "ubuntu",
   "architecture" => "x86_64",
-  "image" => {"publisher"=>"Canonical", "offer"=>"UbuntuServer", "sku"=>"16.04-LTS", "version"=>"16.04.201611220"}
+  # "image" => {"publisher"=>"Canonical", 
+  #   "offer"=>"UbuntuServer", 
+  #   "sku"=>"16.04-LTS", 
+  #   "version"=>"16.04.201611220"
+  # }
 }
-cpi.create_stemcell('', stemcell_properties)
+stempCellName = cpi.create_stemcell(
+  '~/Downloads/bosh-stemcell-2222.22-azure-hyperv-ubuntu-xenial-go_agent/image',
+  stemcell_properties)
 
+puts stempCellName
+# "bosh-stemcell-e412edaf-3a44-46fa-9cac-c667dd232ce8"
 # Test create_vm with a valid stemcell_id
-agent_id = "<GUID>"
-stemcell_id = "<a-valid-stemcell-id>"
-resource_pool = JSON('{"instance_type":"Standard_F1"}')
+agent_id = SecureRandom.uuid
+stemcell_id = stempCellName
+resource_pool = {
+  "instance_type"=>"Standard_A1_v2"
+}
 networks = JSON('{"private":{"cloud_properties":{"subnet_name":"Bosh","virtual_network_name":"boshvnet-crp"},"default":["dns","gateway"],"dns":["168.63.129.16","8.8.8.8"],"gateway":"10.0.0.1","ip":"10.0.0.42","netmask":"255.255.255.0","type":"manual"}}')
 instance_id = cpi.create_vm(agent_id, stemcell_id, resource_pool, networks)
 puts instance_id
